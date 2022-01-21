68e99375a38223987fac4bcf6d2bbee702bf9e8d
Ankit_Anmol <87054411+Quantum-Codes@users.noreply.github.com>
2022-01-21 10:34:04 +0000

    Works

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, send_file
+import random
 
 app = Flask("app")
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 
-@app.route("/")
+
+@app.route("/", methods = ['GET'])
 def form():
-  return render_template("form.html" methods=['GET','POST'])
+  if request.method == "GET":
+    print(request.referrer, random.randint(0,1000))
+    return send_file("clock.png"), 200
+
+@app.route("/email")
+def email():
+  return render_template("index.html")
 
 
-app.run(host='0.0.0.0', port=8080)
+app.run(host='0.0.0.0', port=8080, debug=False)#replit's port
