61fa769f316697885ecd62732ca78cd35ab8d689
Ankit_Anmol <87054411+Quantum-Codes@users.noreply.github.com>
2022-01-21 13:16:56 +0000

    Working

--- main.py
+++ main.py
@@ -7,9 +7,13 @@ app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 
 @app.route("/", methods = ['GET'])
 def form():
+  print("got req")
   if request.method == "GET":
-    print(request.referrer, random.randint(0,1000))
-    return send_file("clock.png"), 200
+    x = random.randint(0,1000)
+    print(request.referrer, x)
+    with open("test.txt", 'a') as file1:
+      file1.write(f"{request.referrer} {x}\n")
+    return send_file("clock.png",cache_timeout=0), 200
 
 @app.route("/email")
 def email():
